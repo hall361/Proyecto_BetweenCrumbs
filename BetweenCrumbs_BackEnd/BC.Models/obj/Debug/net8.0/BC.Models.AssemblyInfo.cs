5a53fdc99d6609d0c6b9b0f928238e9c54e52588
HECTOR ANIBAL LOPEZ LOPEZ <hall361@hotmail.com>
2025-04-27 02:21:28 -0500

    susbir

--- BetweenCrumbs_BackEnd/BC.Models/obj/Debug/net8.0/BC.Models.AssemblyInfo.cs
+++ BetweenCrumbs_BackEnd/BC.Models/obj/Debug/net8.0/BC.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BC.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5aba63709ee7399c0b09fb4751f3ce1009005a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc05ad5522f2c021c512a47bccae7855c99efad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BC.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BC.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
